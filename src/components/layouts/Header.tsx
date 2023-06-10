3ba39fe84f0eda2cb8976e3d26591a5328b001a5
y.k <yukke5222@gmail.com>
2023-06-11 04:25:00 +0900

    🎨 header改修/サービス名とトップリンク設置

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -5,13 +5,16 @@ import {
   SignUpButton,
   UserButton,
 } from '@clerk/nextjs';
+import Link from 'next/link';
 
 import { SearchBar } from '../elements/SearchBar';
 
 export const Header = () => {
   return (
     <header className='flex items-center bg-red-100'>
-      <h1>Header</h1>
+      <Link href='/'>
+        <h1>Virtual Credit</h1>
+      </Link>
       <SearchBar />
       <SignedIn>
         <UserButton afterSignOutUrl='/' />
