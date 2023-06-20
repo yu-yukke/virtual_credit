08a4115cc9c3d7182faaf51264931edb61197956
y.k <yukke5222@gmail.com>
2023-06-21 01:19:14 +0900

    ✨ drizzle schema定義

---
+++ src/db/schema.ts
@@ -0,0 +1,55 @@
+import { InferModel, relations } from 'drizzle-orm';
+import {
+  mysqlTable,
+  varchar,
+  int,
+  index,
+  json,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/mysql-core';
+
+// ********** user account ********** //
+export const users = mysqlTable(
+  'users',
+  {
+    id: int('id').autoincrement().primaryKey(),
+    externalId: varchar('external_id', { length: 255 }).notNull(),
+    attributes: json('attributes').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+  },
+  (users) => ({
+    externalIdIndex: uniqueIndex('externalId_idx').on(users.externalId),
+  }),
+);
+
+export type User = InferModel<typeof users>;
+export type NewUser = InferModel<typeof users, 'insert'>;
+
+export const usersRelations = relations(users, ({ one }) => ({
+  social: one(socials, {
+    fields: [users.id],
+    references: [socials.userId],
+  }),
+}));
+
+// ********** social account ids ********** //
+export const socials = mysqlTable(
+  'socials',
+  {
+    id: int('id').autoincrement().primaryKey(),
+    userId: int('user_id')
+      .references(() => users.id)
+      .notNull(),
+    twitterId: varchar('twitter_id', { length: 256 }).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+  },
+  (socials) => ({
+    userIdIndex: index('userId_idx').on(socials.userId),
+  }),
+);
+
+export type Social = InferModel<typeof socials>;
+export type NewSocial = InferModel<typeof socials, 'insert'>;
