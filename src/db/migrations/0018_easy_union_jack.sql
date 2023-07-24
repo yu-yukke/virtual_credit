605a56dff4b39b763cd193a86d57b75d69146521
y.k <yukke5222@gmail.com>
2023-07-24 12:36:09 +0900

    🗃️ improve/socialテーブルにinstagramとwebsiteカラムを追加

--- src/db/migrations/0018_easy_union_jack.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX `clerkIdProvider_idx` ON `users`;--> statement-breakpoint
-CREATE UNIQUE INDEX `clerkId_idx` ON `users` (`clerk_id`);--> statement-breakpoint
-ALTER TABLE `users` DROP COLUMN `provider`;
---
+++ src/db/migrations/0020_late_brother_voodoo.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `socials` ADD `website_url` varchar(256);--> statement-breakpoint
+ALTER TABLE `socials` DROP COLUMN `website_udl`;
