6b9d51b87930c2addd8df37b5d52a02a1917e67d
y.k <yukke5222@gmail.com>
2023-10-24 14:05:38 +0900

    style: 💄 フィルター用ボタンのホバー時のスタイルを修正

--- src/components/elements/buttons/filter-button.tsx
+++ src/components/elements/buttons/filter-button.tsx
@@ -17,8 +17,6 @@ export const FilterButton = ({ text }: Props) => {
       _hover={{
         color: '#2D2D2E',
         bg: 'white',
-        boxShadow:
-          '0px 1px 2px -1px rgba(41, 44, 49, 0.06), 0px 0px 0px 1px rgba(15, 16, 18, 0.06), 0px 2px 4px 0px rgba(73, 79, 90, 0.04)',
       }}
     >
       {text}
