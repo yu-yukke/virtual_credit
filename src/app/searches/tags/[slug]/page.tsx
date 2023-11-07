48b62a6a2757557573a6fef9d142b381a4ff09b5
y.k <yukke5222@gmail.com>
2023-11-08 02:58:42 +0900

    style: 💄 作品数を表示するよう修正

--- src/app/searches/tags/[slug]/page.tsx
+++ src/app/searches/tags/[slug]/page.tsx
@@ -2,17 +2,48 @@ import { Spacer, VStack } from '@kuma-ui/core';
 import { WorkList } from './_components';
 import { Categories, Tags } from '@/app/works/_components';
 import { PageHeadingWrapper } from '@/components/layouts/page-heading-wrapper';
+import prisma from '@/lib/prisma';
 
 type Props = {
   slug: string;
 };
 
-export default function Page({ params }: { params: Props }) {
+export default async function Page({ params }: { params: Props }) {
+  const filteredWorks = await prisma.work.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    where: {
+      workTags: {
+        some: {
+          tag: {
+            name: params.slug,
+          },
+        },
+      },
+    },
+    include: {
+      histories: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+      },
+      workCategories: {
+        include: {
+          category: true,
+        },
+      },
+    },
+  });
+  const works = await filteredWorks.filter(
+    (work) => !!work.histories.length && work.histories[0].published,
+  );
+
   return (
     <>
       <PageHeadingWrapper
         title={`# ${decodeURI(params.slug)}`}
-        description={`A collection of ${params.slug} works`}
+        description={`A collection of ${works.length} works`}
       />
       <Spacer size={1} bg={'colors.borderPrimary'} className='full-bleed' />
       <VStack as='section' mt={20}>
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -10,11 +10,31 @@ import {
   WorkList,
 } from './_components';
 import { PageHeadingWrapper } from '@/components/layouts/page-heading-wrapper';
+import prisma from '@/lib/prisma';
 
-export default function Page() {
+export default async function Page() {
+  const worksWithHistories = await prisma.work.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      histories: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+      },
+    },
+  });
+  const works = await worksWithHistories.filter(
+    (work) => !!work.histories.length && work.histories[0].published,
+  );
+
   return (
     <>
-      <PageHeadingWrapper title='Works' description={`A collection of works`} />
+      <PageHeadingWrapper
+        title='Works'
+        description={`A collection of ${works.length} works`}
+      />
       <Spacer size={1} bg={'colors.borderPrimary'} className='full-bleed' />
       <VStack as='section' mt={20}>
         <Suspense fallback={<SkeletonCategories />}>
