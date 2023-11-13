c3ff4878912645ae36a02ef844b166ebd139645f
y.k <yukke5222@gmail.com>
2023-11-14 01:53:58 +0900

    style: 💄 トップのリンクをトリ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import { Box } from '@kuma-ui/core';
-import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <Box>
-      <div>
-        <Link href='/works'>作品一覧</Link>
-      </div>
-      <div>
-        <Link href='/creators'>クリエイター一覧</Link>
-      </div>
-    </Box>
-  );
+  return <Box>{'トップページ'}</Box>;
 }
