256a520ba03b3b87914f96a550c3f2fe55540d9f
y.k <yukke5222@gmail.com>
2024-08-31 23:48:40 +0900

    style: 💄 作品カードのレイアウトを変更 #169

--- src/components/elements/cards/work-card.tsx
+++ src/components/elements/cards/work-card.tsx
@@ -1,25 +1,29 @@
-import Image from 'next/image'
+'use client'
+
+import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
 
 import { Work } from '@/types/work'
 
 export const WorkCard = ({ work }: { work: Work }) => {
   return (
-    <div className='group'>
-      <div className='relative p-4 overflow-hidden transition-all duration-500 rounded-md aspect-video bg-tertiary/5 group-hover:shadow-md'>
-        <Image
-          src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${work.coverImageUrl}`}
-          alt={work.title}
-          fill
-          sizes='100%'
-          className='object-cover transition-all duration-500 group-hover:scale-105'
-          loading='lazy'
-        />
-      </div>
-      <div className='px-1 py-2'>
-        <p className='text-sm tracking-wider truncate text-foreground'>
+    <Card className='flex-col-reverse pt-2 pb-4 group'>
+      <CardHeader className='flex-col items-start px-4 pt-2 pb-0'>
+        <small className='text-xs text-tertiary'>
+          {work.userCount} Creators
+        </small>
+        <h2 className='w-full text-base font-bold tracking-wider truncate text-foreground'>
           {work.title}
-        </p>
-      </div>
-    </div>
+        </h2>
+      </CardHeader>
+      <CardBody className='py-2 overflow-visible'>
+        <div className='relative overflow-hidden rounded-xl'>
+          <Image
+            alt={work.title}
+            className='object-cover transition-all duration-500 group-hover:scale-105'
+            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${work.coverImageUrl}`}
+          />
+        </div>
+      </CardBody>
+    </Card>
   )
 }
