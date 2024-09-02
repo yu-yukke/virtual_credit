99fcfac04c58e54c980357cf6822f5ca4fbf4360
y.k <yukke5222@gmail.com>
2024-09-03 00:11:58 +0900

    mod: 🔧 作品一覧で使うコンポーネントの型の引用先を修正

--- src/app/works/_components/work-list.tsx
+++ src/app/works/_components/work-list.tsx
@@ -1,13 +1,13 @@
-import { Work } from '@/types/work'
+import { SimpleWork } from '@/types/work'
 
 import { WorkCard } from '@/components/elements/cards'
 import Link from 'next/link'
 
-export const WorkList = ({ works }: { works: Work[] }) => {
+export const WorkList = ({ works }: { works: SimpleWork[] }) => {
   return (
     <div className='grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-2 lg:grid-cols-3'>
       {works.map((work) => (
-        <Link href='#' key={work.id}>
+        <Link href={`/works/${work.id}`} key={work.id}>
           <WorkCard work={work} />
         </Link>
       ))}
