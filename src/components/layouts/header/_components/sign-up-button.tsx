c326c91ff55e239a1e3b3d2b3181ce9c8b35b811
y.k <yukke5222@gmail.com>
2024-09-13 02:26:53 +0900

    style: 💄 サインアップのホバー時のCSSを修正 #172

--- src/components/layouts/header/_components/sign-up-button.tsx
+++ src/components/layouts/header/_components/sign-up-button.tsx
@@ -5,7 +5,7 @@ export const SignUpButton = () => {
     <Button
       variant='light'
       radius='full'
-      className='px-4 text-sm tracking-wide text-white cursor-pointer bg-gradient-to-tr from-sky-500 to-fuchsia-500 hover:top-sky-400 hover:to-fuchsia-400 hover:shadow-sm'
+      className='px-4 text-sm tracking-wide text-white transition-all cursor-pointer duration-400 bg-gradient-to-tr from-sky-500 to-fuchsia-500 hover:opacity-90 hover:shadow-sm'
     >
       Sign Up
     </Button>
