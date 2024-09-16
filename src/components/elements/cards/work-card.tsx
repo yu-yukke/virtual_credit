fd73ce419b295dc6d9ba415ead26bab502978f4d
y.k <yukke5222@gmail.com>
2024-09-17 03:05:18 +0900

    mod: 🔧 作品一覧のカードレイアウトを修正 #178

--- src/components/elements/cards/work-card.tsx
+++ src/components/elements/cards/work-card.tsx
@@ -1,29 +1,27 @@
-'use client'
-
-import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
+import Image from 'next/image'
 
 import { SimpleWork } from '@/types/work'
 
 export const WorkCard = ({ work }: { work: SimpleWork }) => {
   return (
-    <Card className='flex-col-reverse pt-2 pb-4 group'>
-      <CardHeader className='flex-col items-start px-4 pt-2 pb-0'>
-        <small className='text-xs text-tertiary'>
-          {work.userCount} Creators
-        </small>
-        <h2 className='w-full text-base font-bold tracking-wider truncate text-foreground'>
+    <div className='group'>
+      <div className='relative overflow-hidden transition-all duration-500 rounded-sm aspect-video group-hover:shadow-xl'>
+        <Image
+          alt={work.title}
+          className='object-cover transition-opacity duration-500 group-hover:opacity-90'
+          src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${work.coverImageUrl}`}
+          fill
+          sizes='100%'
+        />
+      </div>
+      <div className='px-1 py-2'>
+        <h2 className='text-lg font-bold tracking-wider truncate'>
           {work.title}
         </h2>
-      </CardHeader>
-      <CardBody className='py-2 overflow-visible'>
-        <div className='relative overflow-hidden rounded-xl'>
-          <Image
-            alt={work.title}
-            className='object-cover transition-all duration-500 group-hover:scale-105'
-            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${work.coverImageUrl}`}
-          />
-        </div>
-      </CardBody>
-    </Card>
+        <p className='text-xs tracking-wide text-tertiary'>
+          {work.userCount} Creators
+        </p>
+      </div>
+    </div>
   )
 }
